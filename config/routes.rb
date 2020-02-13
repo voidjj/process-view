f8f436414bd32f3bb86d7ac97dfa9fb787050472
jjswski i <jjswski@gmail.com>
2020-02-13 21:25:14 +0100

    Add Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
